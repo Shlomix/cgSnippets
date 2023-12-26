9d9c7f43c4999e39115d5202e44cd551408fbb1f
Shlomix <shlomix@gmail.com>
2023-12-26 12:33:01 +0200

    Create adam_simplified.py

---
+++ adam_simplified.py
@@ -0,0 +1,85 @@
+import math
+import torch
+from torch.optim.optimizer import Optimizer
+from typing import List, Optional
+
+class Adam(Optimizer):
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
+                 weight_decay=0, amsgrad=False, *, maximize: bool = False):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+        if not 0.0 <= weight_decay:
+            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+
+        defaults = dict(lr=lr, betas=betas, eps=eps,
+                        weight_decay=weight_decay, amsgrad=amsgrad,
+                        maximize=maximize)
+        super(Adam, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super().__setstate__(state)
+        for group in self.param_groups:
+            group.setdefault('amsgrad', False)
+            group.setdefault('maximize', False)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step."""
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is not None:
+                    grad = p.grad
+                    if grad.is_sparse:
+                        raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
+                    if group['maximize']:
+                        grad = -grad
+
+                    state = self.state[p]
+
+                    # State initialization
+                    if len(state) == 0:
+                        state['step'] = 0
+                        state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                        state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                        if group['amsgrad']:
+                            state['max_exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                    exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                    if group['amsgrad']:
+                        max_exp_avg_sq = state['max_exp_avg_sq']
+
+                    state['step'] += 1
+                    beta1, beta2 = group['betas']
+
+                    if group['weight_decay'] != 0:
+                        grad.add_(p, alpha=group['weight_decay'])
+
+                    # Update biased first moment estimate
+                    exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                    # Update biased second raw moment estimate
+                    exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+
+                    if group['amsgrad']:
+                        # Maintains the maximum of all 2nd moment running avg. till now
+                        torch.maximum(max_exp_avg_sq, exp_avg_sq, out=max_exp_avg_sq)
+                        # Use the max. for normalizing running avg. of gradient
+                        denom = (max_exp_avg_sq.sqrt() / math.sqrt(1 - beta2 ** state['step'])).add_(group['eps'])
+                    else:
+                        denom = (exp_avg_sq.sqrt() / math.sqrt(1 - beta2 ** state['step'])).add_(group['eps'])
+
+                    step_size = group['lr'] / (1 - beta1 ** state['step'])
+
+                    p.addcdiv_(exp_avg, denom, value=-step_size)
+
+        return loss
