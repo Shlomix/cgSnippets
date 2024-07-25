7bb19658ce60fc181f9b99b1f3673f1b1db0658d
Shlomix <shlomix@gmail.com>
2024-07-25 10:57:35 +0300

    Update download_hf.sh

--- download_hf.sh
+++ download_hf.sh
@@ -30,22 +30,25 @@ fi
 
 model_name=$1
 
+# Use a safe directory name for cloning
+safe_dir_name="model_clone_dir"
+
 # Command to clone the repo and download the bin files
 cmd=$(cat <<EOF
 #!/bin/bash
-GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/${model_name}
+GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/${model_name} ${safe_dir_name}
 if [[ $? -ne 0 ]]; then
     echo "Failed to clone repository https://huggingface.co/${model_name}"
     exit 1
 fi
-cd "${model_name}" || exit 1
+cd "${safe_dir_name}" || exit 1
 $(declare -f download_bin_files)
 download_bin_files "${model_name}"
 EOF
 )
 
 # Start a new tmux session and run the command
-tmux new-session -d -s download_session "$cmd"
+tmux new-session -s download_session -d "bash -c \"$cmd\""
 
 # Attach to the tmux session
-tmux attach -t download_session
+tmux attach-session -t download_session
